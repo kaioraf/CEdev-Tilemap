24cd00eb4ab01b5ca86f335344b64d7dd5001444
kaioraf <45394248+kaioraf@users.noreply.github.com>
2023-01-26 13:44:33 +0100

    It works now!!!!!!!!!!!

--- src/tilemap.cpp
+++ src/tilemap.cpp
@@ -12,7 +12,7 @@
 
 /* Include the external tilemap data */
 // extern unsigned char tilemap_map[];
-extern unsigned char snowLevel_1_map[];
+extern unsigned char tilemap_map[];
 extern unsigned char mainMenu_map[];
 
 /* Tilemap defines */
@@ -288,8 +288,49 @@ bool isWalkable(int tileID)
 {
     switch (tileID)
     {
+    case 15:
+    case 16:
+    case 17:    
+    case 18:
+    case 19:
     case 21:
     case 22:
+    case 23:
+    case 29:
+    case 30:
+    case 31:
+    case 32:
+    case 33:
+    case 35:
+    case 36:
+    case 37:
+    case 38:
+    case 39:
+    case 41:
+    case 42:
+    case 43:
+    case 45:
+    case 46:
+    case 47:
+    case 48:
+    case 55:
+    case 56:
+    case 57:
+    case 58:
+    case 59:
+    case 65:
+    case 66:
+    case 67:
+    case 68:
+    case 88:
+    case 98:
+    case 99:
+    case 101:
+    case 108:
+    case 113:
+    case 116:
+    case 128:
+    case 133:
         return true;
         break;
     
@@ -508,7 +549,7 @@ int main(void)
 
     GraphxSetup();
 
-    Tilemap *snowLevel_1 = new Tilemap(snowLevel_1_map, snowSet1_tiles, gfx_tile_16_pixel, TILE_HEIGHT, snowLevel1_DRAW_WIDTH, snowLevel1_DRAW_HEIGHT, snowLevel1_HEIGHT, snowLevel1_WIDTH, snowLevel1_X_OFFSET, snowLevel1_Y_OFFSET);
+    Tilemap *snowLevel_1 = new Tilemap(tilemap_map, snowSet1_tiles, gfx_tile_16_pixel, TILE_HEIGHT, snowLevel1_DRAW_WIDTH, snowLevel1_DRAW_HEIGHT, snowLevel1_HEIGHT, snowLevel1_WIDTH, snowLevel1_X_OFFSET, snowLevel1_Y_OFFSET);
 
     playerBackground->width = Player_width;
     playerBackground->height = Player_height;
@@ -535,12 +576,12 @@ int main(void)
 
         kb_Scan();
         arrows = kb_Data[7];
-        // input(arrows, snowLevel_1, PlayerSprite);
-        // currentTile = ConvertSpritePosition(snowLevel_1, PlayerSprite);
+        input(arrows, snowLevel_1, PlayerSprite);
+        currentTile = ConvertSpritePosition(snowLevel_1, PlayerSprite);
 
         if (timeSinceLastUpdate > 1092) //update 30 times per second
         {
-            // update(snowLevel_1 , PlayerSprite, timeSinceLastUpdate, currentTile);
+            update(snowLevel_1 , PlayerSprite, timeSinceLastUpdate, currentTile);
             lastUpdate = clockTime;
 
             if (os_GetCSC() == sk_Mode) {menuOpen = true;}
